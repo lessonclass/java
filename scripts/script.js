c9f0201de89a385a8762e3409717ce4b3cab6a5f
Lessonclass <amantur.k764@gmail.com>
2022-11-21 16:12:20 +0300

    java script task

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+console.log('Hello world')
+console.log('My name is Amantur')
+
+const num = 29
+console.log('29')
+
+const a = 10
+const b = 2
+const c = 5
+console.log (a + b + c)
+
+const d = 100
+console.log(d * 2)
+
+const e = 67
+console.log(e % 7)
+
+const f = 10
+const i = 2
+console.log(f + i)
+console.log(f - i)
+console.log(f * i)
+console.log(f / i)
+
+const k = 15
+const l = 2
+console.log(k + l)
+const result = 17
+console.log(result) //('result')
+
+const m = 30
+const n = 15
+console.log(m + n)
+const c1 = 45
+console.log(c1 / 4)
+
+
+const a1 = 17
+const b1 = 10
+console.log(a1 - b1)
+const c2 = 7
+const d2 = 16
+console.log(c2 * d2)
+const result1 = 112
+console.log( result1 )
